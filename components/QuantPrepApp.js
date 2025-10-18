62db9e0162251550ab04cd1bd3d082b50e7ad5ce
prithvibhardwaj <prithvi.p.bhardwaj@gmail.com>
2025-10-18 16:57:25 +0800

    Edit

--- components/QuantPrepApp.js
+++ components/QuantPrepApp.js
@@ -3528,7 +3528,7 @@ const evaluateAnswer = async (userAnswer, correctAnswer, question) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+        "Authorization": `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
       },
       body: JSON.stringify({
         model: "gpt-4o",
